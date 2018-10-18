adb58992ee0ce0ab851a95536df3f1eb098c4e10
Russell Chan <russellchanweili@gmail.com>
2018-10-18 17:29:48 +0300

    Update Get_RT_Lena.py
    Update code from Lena for RT extraction.  Have not tried to

--- Get_RT_Lena.py
+++ Get_RT_Lena.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 """
 Created on Tue Aug 28 17:12:36 2018
 
 @author: Zou-Williams, L.
 """
+
 
 import mne
 import numpy as np
@@ -10,3 +13,16 @@ import os.path as op
 import glob
 import csv
 mne.set_log_level('ERROR')
+
+
+
+def rt(set_file):
+    raw  = mne.io.read_raw_brainvision(set_file,eog=['SO1', 'IO1', 'LO1', 'LO2'],montage='standard_1020',preload=True)
+    events = mne.find_events(raw)
+    t = events[:,0].tolist()
+    r = events[:,2].tolist()
+    rt = zip(t,r)
+    with open('rt/' + set_file + '.csv', "w") as f:
+        writer = csv.writer(f)
+        for row in rt:
+            writer.writerow(row)    
