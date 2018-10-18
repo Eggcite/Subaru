96cd6e63ed144e30381e124eca5b587348bb7ed9
Russell Chan <russellchanweili@gmail.com>
2018-10-18 15:38:11 +0300

    Create Preproc_Subaru.py
    Preproc and Proc for Subaru project.

---
+++ Preproc_Subaru.py
@@ -0,0 +1,12 @@
+"""
+Created on Tue Aug 28 17:12:36 2018
+
+@author: Zou-Williams, L. & Chan, R.W.
+"""
+
+import mne
+import numpy as np
+import os.path as op
+import glob
+import csv
+mne.set_log_level('ERROR')
